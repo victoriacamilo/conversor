11fe460d9e39474a1ddd725d38ebefc98e574dfc
victoriacamilo <victoria.neves.camilo@gmail.com>
2022-09-07 19:23:21 -0300

    ....

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function converter() {
+    var valorElemento= document.getElementById ("valor");
+    var valor= valorElemento.value;
+    var valorEmDolarNumerico = parseFloat(valor);
+  console.log("valorEmDolarNumerico");
+}
